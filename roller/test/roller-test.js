22bff5e30a4ed946b916a9de0193cdf40fb85437
Donovan Mikrot <donovan@donovan.mn>
2016-03-13 16:24:49 -0500

    Add additional unit tests and cover error handling

---
+++ roller/test/roller-test.js
@@ -0,0 +1,88 @@
+var Roller = require('../slackbot/roller');
+var roller = new Roller();
+var chai = require('chai');
+var expect = chai.expect;
+
+chai.use(require('dirty-chai'));
+
+describe('roller', function () {
+  var assertResult = function (result, roll, min, max) {
+    expect(result).to.have.property('roll');
+    expect(result).to.have.property('total');
+    expect(result.roll).to.eq(roll);
+    if (max !== undefined) {
+      expect(result.total).to.be.within(min, max);
+    } else {
+      expect(result.total).to.eq(min);
+    }
+  };
+
+  it('assumes a single dice if how many to roll is missing', function () {
+    var result = roller.roll('d4');
+
+    assertResult(result, 'd4', 1, 4);
+  });
+
+  it('can add', function () {
+    var result = roller.roll('1d4+4');
+
+    assertResult(result, '1d4+4', 5, 8);
+  });
+
+  it('can multiply', function () {
+    var result = roller.roll('1d4*2');
+
+    assertResult(result, '1d4*2', 2, 8);
+  });
+
+  it('can subtract', function () {
+    var result = roller.roll('1d4-1');
+
+    assertResult(result, '1d4-1', 0, 3);
+  });
+
+  it('can divide', function () {
+    var result = roller.roll('1d10/2');
+
+    assertResult(result, '1d10/2', 0, 5);
+  });
+
+  it('can modulo', function () {
+    var result = roller.roll('1d10%2');
+
+    assertResult(result, '1d10%2', 0, 1);
+  });
+
+  it('rounds down', function () {
+    var result = roller.roll('9/2');
+
+    assertResult(result, '9/2', 4);
+  });
+
+  it('can roll many dice', function () {
+    var result = roller.roll('100d4');
+
+    assertResult(result, '100d4', 100, 400);
+  });
+
+  it('gives random results', function () {
+    var firstResult = roller.roll('10d10000');
+    var secondResult = roller.roll('10d10000');
+
+    assertResult(firstResult, '10d10000', 10, 100000);
+    assertResult(secondResult, '10d10000', 10, 100000);
+    expect(firstResult.total).to.not.eq(secondResult.total);
+  });
+
+  it('sanitizes unexpected characters', function () {
+    var result = roller.roll(';eval(10.0[d]4+&2!0)');
+
+    assertResult(result, '(100d4+20)', 120, 420);
+  });
+
+  it('throws error if roll is empty after sanitizing', function () {
+    expect(function () {
+      roller.roll('empty');
+    }).to.throw(/Can't calculate empty roll/);
+  });
+});
--- roller/test/slackbot-test.js
+++ roller/test/slackbot-test.js
@@ -28,7 +28,7 @@ describe('root command', function () {
     expect(received).to.be.true();
     expect(receivedArgs[0]).to.eq(null);
     expect(receivedArgs[1].response_type).to.eq('in_channel');
-    expect(receivedArgs[1].text).to.match(/testUser rolls 2d4\+1 and gets [2-9]/);
+    expect(receivedArgs[1].text).to.match(/testUser rolls 2d4\+1 and gets [3-9]/);
   });
 
   it('responds with multiple results', function () {
@@ -47,7 +47,7 @@ describe('root command', function () {
     lines = receivedArgs[1].text.split('\n');
     expect(lines.length).to.eq(2);
     for (i = 0; i < lines.length; ++i) {
-      expect(lines[i]).to.match(/testUser rolls 2d4\+1 and gets [2-9]/);
+      expect(lines[i]).to.match(/testUser rolls 2d4\+1 and gets [3-9]/);
     }
   });
 
@@ -66,5 +66,21 @@ describe('root command', function () {
 
     expect(received).to.be.true();
     expect(receivedArgs).to.deep.eq(helpArgs);
+  });
+
+  it('responds with error message for invalid rolls', function () {
+    var error = 'You asked me to roll "2d8", "+", "1".\nI couldn\'t figure out how to roll "+".';
+
+    slackBot.root({
+      args: {
+        rolls: ['2d8', '+', '1']
+      },
+      userName: 'testUser'
+    }, callback);
+
+    expect(received).to.be.true();
+    expect(receivedArgs[0]).to.eq(null);
+    expect(receivedArgs[1].response_type).to.eq('ephemeral');
+    expect(receivedArgs[1].text).to.eq(error);
   });
 });
